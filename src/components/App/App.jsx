3e60a9bb3ff00f9dc79a039dc1e0b96bd7dfa67a
danielVFS <daniel.vitor@cuia.com.br>
2022-04-15 16:21:49 -0300

    chore: remove unused useState

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import LineChart from "../../shared/LineChart";
 import AppContainer from "../AppContainer/AppContainer";
 import AppHeader from "../AppHeader";
